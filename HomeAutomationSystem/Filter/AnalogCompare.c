67b00e20dde1d62a7a34d0332ece14abf83865ae
vbBerni <Bernhad@gmx.at>
2015-01-03 13:31:02 +0100

    first checkin

---
+++ HomeAutomationSystem/Filter/AnalogCompare.c
@@ -0,0 +1,28 @@
+/*
+ * AnalogCompare.c
+ *
+ * Created: 11.11.2014 18:53:02
+ *  Author: Bernhard
+ */ 
+#include "AnalogCompare.h"
+
+#include <stdbool.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+void* analogCompare_process(void* arg)
+{
+	uint16_t value = (uint16_t)arg;
+	
+	printf("%d\r\n", value);
+	
+	if (value > 128)
+	{
+		return (void*)1;
+	}
+	else
+	{
+		return (void*)0;
+	}
+}
---
+++ HomeAutomationSystem/Filter/BinaryOutFilter.h
@@ -0,0 +1,14 @@
+/*
+ * BinaryOut.h
+ *
+ * Created: 01.11.2014 21:54:02
+ *  Author: Bernhard
+ */ 
+
+
+#ifndef BINARYOUTFILTER_H_
+#define BINARYOUTFILTER_H_
+
+void* binaryOut_process(void* arg);
+
+#endif /* BINARYOUTFILTER_H_ */
