e7c27fd4d75d3b32c321de2e965e4948e52ddcbb
Kevin <kevin.wallis@hotmail.com>
2015-08-12 21:50:25 +0200

    Moved canvas to a own control for drag drop.

---
+++ HASystem/HASystem.Desktop.Presentation/Controls/DropContainer.xaml.cs
@@ -0,0 +1,38 @@
+﻿using HASystem.Desktop.Presentation.Utils;
+using System.Windows.Controls;
+
+namespace HASystem.Desktop.Presentation.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für DropContainer.xaml
+    /// </summary>
+    public partial class DropContainer : UserControl
+    {
+        #region fields
+        private DrawingHelper helper;
+        #endregion
+
+        public DropContainer()
+        {
+            InitializeComponent();
+            helper = new DrawingHelper();
+        }
+
+        #region methods
+        private void OnMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var control = new Gate();
+            var position = e.MouseDevice.GetPosition(designer);
+
+            Canvas.SetTop(control, position.Y);
+            Canvas.SetLeft(control, position.X);
+
+            if (!helper.CollisionExists(control))
+            {
+                helper.AddControl(control);
+                designer.Children.Add(control);
+            }
+        }
+        #endregion
+    }
+}
