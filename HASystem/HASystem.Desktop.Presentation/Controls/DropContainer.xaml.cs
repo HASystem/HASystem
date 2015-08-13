94f5708d1954c361a49015be607829db62ac3c69
Kevin <kevin.wallis@hotmail.com>
2015-08-13 18:07:10 +0200

    Tried drag drop stuff.

--- HASystem/HASystem.Desktop.Presentation/Controls/DropContainer.xaml.cs
+++ HASystem/HASystem.Desktop.Presentation/Controls/DropContainer.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using HASystem.Desktop.Presentation.Utils;
 using System.Windows.Controls;
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
 
 namespace HASystem.Desktop.Presentation.Controls
 {
@@ -10,29 +15,73 @@ namespace HASystem.Desktop.Presentation.Controls
     {
         #region fields
         private DrawingHelper helper;
+
+        public bool IsDragging { get; private set; }
         #endregion
 
         public DropContainer()
         {
             InitializeComponent();
             helper = new DrawingHelper();
+
         }
 
         #region methods
-        private void OnMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void panel_DragOver(object sender, DragEventArgs e)
         {
-            var control = new Gate();
-            var position = e.MouseDevice.GetPosition(designer);
-
-            Canvas.SetTop(control, position.Y);
-            Canvas.SetLeft(control, position.X);
-
-            if (!helper.CollisionExists(control))
+            if (e.Data.GetDataPresent("Object"))
             {
-                helper.AddControl(control);
-                designer.Children.Add(control);
+                // These Effects values are used in the drag source's
+                // GiveFeedback event handler to determine which cursor to display.
+                if (e.KeyStates == DragDropKeyStates.ControlKey)
+                {
+                    e.Effects = DragDropEffects.Copy;
+                }
+                else
+                {
+                    e.Effects = DragDropEffects.Move;
+                }
             }
         }
+
+        private void panel_Drop(object sender, DragEventArgs e)
+        {
+            // If an element in the panel has already handled the drop,
+            // the panel should not also handle it.
+            if (e.Handled == false)
+            {
+                Panel _panel = (Panel)sender;
+                UIElement _element = (UIElement)e.Data.GetData("Object");
+
+                if (_panel != null && _element != null)
+                {
+                    // Get the panel that the element currently belongs to,
+                    // then remove it from that panel and add it the Children of
+                    // the panel that its been dropped on.
+                    Panel _parent = (Panel)VisualTreeHelper.GetParent(_element);
+
+                    if (_parent != null)
+                    {
+                        if (e.KeyStates == DragDropKeyStates.ControlKey &&
+                            e.AllowedEffects.HasFlag(DragDropEffects.Copy))
+                        {
+                            Gate _circle = new Gate((Gate)_element);
+                            _panel.Children.Add(_circle);
+                            // set the value to return to the DoDragDrop call
+                            e.Effects = DragDropEffects.Copy;
+                        }
+                        else if (e.AllowedEffects.HasFlag(DragDropEffects.Move))
+                        {
+                            _parent.Children.Remove(_element);
+                            _panel.Children.Add(_element);
+                            // set the value to return to the DoDragDrop call
+                            e.Effects = DragDropEffects.Move;
+                        }
+                    }
+                }
+            }
+        }
+
         #endregion
     }
 }
