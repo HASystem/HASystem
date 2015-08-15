351227534f97550e54a41f9600945ad408691132
Kevin <kevin.wallis@hotmail.com>
2015-08-15 08:16:18 +0200

    Added drag drop container.

---
+++ HASystem/HASystem.Desktop.Presentation/Controls/Gate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HASystem.Desktop.Presentation.Controls
+{
+    /// <summary>
+    /// Interaktionslogik für Gate.xaml
+    /// </summary>
+    public partial class Gate : UserControl
+    {
+        public Gate()
+        {
+            InitializeComponent();
+        }
+    }
+}
